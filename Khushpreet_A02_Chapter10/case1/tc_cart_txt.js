444cb0ae1e870c9353a97f8cae47905345253405
khushpreetsingh25 <114405425+khushpreetsingh25@users.noreply.github.com>
2023-02-06 20:10:48 -0400

    Add files via upload

---
+++ Khushpreet_A02_Chapter10/case1/tc_cart_txt.js
@@ -0,0 +1,14 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5 and CSS3, 8th Edition
+   Tutorial 10
+   Case Problem 1
+
+   Author: Khushpreet singh
+   Date:   january 28,2023
+   
+   Filename: tc_cart.js
+	
+*/
+
